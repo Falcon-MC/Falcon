e530e8d45b2d3cd7a81a1fb0b95f70e147db1530
NaySurGithub <96527285+NaySurGithub@users.noreply.github.com>
2026-09-21 23:08:00 +0200

    fix: describe the seed command

--- Falcon.Server/src/Command/SeedCommand.cpp
+++ Falcon.Server/src/Command/SeedCommand.cpp
@@ -4,7 +4,7 @@
 #include "Network/Handler/ServerNetworkHandler.h"
 
 SeedCommand::SeedCommand(ServerNetworkHandler &handler)
-        : Command("seed", "commands.seed.description", "/seed"), mHandler(handler) {}
+        : Command("seed", "Shows the world seed", "/seed"), mHandler(handler) {}
 
 bool SeedCommand::execute(CommandOrigin &sender, const std::vector<std::string> &arguments) {
     (void) arguments;
