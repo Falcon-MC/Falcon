08677b8c627d8472660898637f727410374c5bd5
NaySurGithub <96527285+NaySurGithub@users.noreply.github.com>
2026-09-21 17:24:05 +0200

    chore: target minecraft 1.26.51

--- Falcon.Server/include/Server.h
+++ Falcon.Server/include/Server.h
@@ -8,7 +8,7 @@ struct ServerSettings {
     int maxPlayers = 20;
     std::string motd = "Falcon Server";
     std::string subMotd = "Falcon";
-    std::string gameVersion = "1.26.50";
+    std::string gameVersion = "1.26.51";
     int protocolVersion = 2193;
     bool runSetupWizard = true;
 };
